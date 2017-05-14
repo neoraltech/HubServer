ed7b00940b408e7b261cc494b8670362cd842941
JohnGuiller <neoraltech>
2017-05-15 00:44:45 +0800

    CORS enabled!

--- WebCamHandler/WebCamHandler/Startup.cs
+++ WebCamHandler/WebCamHandler/Startup.cs
@@ -1,5 +1,7 @@
 ﻿using Owin;
 using Microsoft.Owin;
+using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Cors;
 
 [assembly: OwinStartup(typeof(WebCamHandler.Startup))]
 namespace WebCamHandler
@@ -9,7 +11,11 @@ namespace WebCamHandler
         public void Configuration(IAppBuilder app)
         {
             // Any connection or hub wire up and configuration should go here
-            app.MapSignalR();
+            app.UseCors(CorsOptions.AllowAll);
+
+            HubConfiguration config = new HubConfiguration();
+            config.EnableJSONP = true;
+            app.MapSignalR(config);
         }
     }
 }
